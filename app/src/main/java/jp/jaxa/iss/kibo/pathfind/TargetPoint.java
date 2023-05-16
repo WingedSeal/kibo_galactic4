b965e42364fe9e805492abaf37e8c0ee988b0b1f
WingedSeal <firm09719@gmail.com>
2023-05-17 01:16:48 +0700

    feat(pathfind): add target number and point number as class attribute

--- app/src/main/java/jp/jaxa/iss/kibo/pathfind/TargetPoint.java
+++ app/src/main/java/jp/jaxa/iss/kibo/pathfind/TargetPoint.java
@@ -1,21 +1,28 @@
 package jp.jaxa.iss.kibo.pathfind;
 
 public class TargetPoint extends PathFindNode {
-    public TargetPoint(double x, double y, double z, POI_Id id) {
+    private final int pointNumber;
+
+    public TargetPoint(double x, double y, double z, POI_Id id, int pointNumber) {
         super(x, y, z, id);
+        this.pointNumber = pointNumber;
     }
 
     private static final TargetPoint[] TARGET_POINTS = {
-            new TargetPoint(11.2746, -9.92284, 5.2988, POI_Id.POINT_1),
-            new TargetPoint(10.612, -9.0709, 4.48, POI_Id.POINT_2),
-            new TargetPoint(10.71, -7.7, 4.48, POI_Id.POINT_3),
-            new TargetPoint(10.51, -6.7185, 5.1804, POI_Id.POINT_4),
-            new TargetPoint(11.114, -7.9756, 5.3393, POI_Id.POINT_5),
-            new TargetPoint(11.355, -8.9929, 4.7818, POI_Id.POINT_6),
-            new TargetPoint(11.369, -8.5518, 4.48, POI_Id.POINT_7),
+            new TargetPoint(11.2746, -9.92284, 5.2988, POI_Id.POINT_1, 1),
+            new TargetPoint(10.612, -9.0709, 4.48, POI_Id.POINT_2, 2),
+            new TargetPoint(10.71, -7.7, 4.48, POI_Id.POINT_3, 3),
+            new TargetPoint(10.51, -6.7185, 5.1804, POI_Id.POINT_4, 4),
+            new TargetPoint(11.114, -7.9756, 5.3393, POI_Id.POINT_5, 5),
+            new TargetPoint(11.355, -8.9929, 4.7818, POI_Id.POINT_6, 6),
+            new TargetPoint(11.369, -8.5518, 4.48, POI_Id.POINT_7, 7 ),
     };
 
     public static TargetPoint getTargetPoint(int pointNumber) {
         return TARGET_POINTS[pointNumber - 1];
+    }
+
+    public int getPointNumber() {
+        return pointNumber;
     }
 }
