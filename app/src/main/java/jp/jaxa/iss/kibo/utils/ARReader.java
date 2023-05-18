3ca216f5648c3452e7bc058cba302b629fc0b0eb
Kasidit Sarnrak <otto.ksarnrak@gmail.com>
2023-05-18 21:06:04 +0700

    fix(ARReader) : modify and set method for ARReader class

--- app/src/main/java/jp/jaxa/iss/kibo/utils/ARReader.java
+++ app/src/main/java/jp/jaxa/iss/kibo/utils/ARReader.java
@@ -12,48 +12,36 @@ import jp.jaxa.iss.kibo.rpc.api.KiboRpcApi;
 public class ARReader {
 
     /**
-     * read AR on the target
-     *
-     *
-     *
-     * Numbers of TargetsID start from the upper right
+     * Mark that Numbers of TargetsID start from the upper right
      * and increase in a counterclockwise direction.
      *
+     * read AR from the Navcam type Mat
+     * get ID from
      *
      *
-     *
-     *
+     * @param api KiboRpcApi
+     * @return int[]
      */
 
-    static Mat matSrc;						// Variable store matrix of image.
-    static Mat IDs; 						// Variable store ID each of AR code. : Matrix format
 
-    static List<Mat> Corners = new ArrayList<>(); 	// Variable store four corner each of AR Code.
-    static Dictionary Dict = Aruco.getPredefinedDictionary(Aruco.DICT_5X5_250); // Standard AR code format according rule book.
-    static int[] AR_ID = new int[]{0, 0, 0, 0};		// Variable store ID each of AR code. : Array of Int format
-
-    /* Target tag variables */
-    public int[]   ArID1_CornerRT = new int[]{0, 0};	// Container the specified corner of the ar tag.
-    public int[]   ArID2_CornerLT = new int[]{0, 0};
-    public int[]   ArID3_CornerLB = new int[]{0, 0};
-    public int[]   ArID4_CornerRB = new int[]{0, 0};
-    int ID_1 = 1;
-    int ID_2 = 2;
-    int ID_3 = 3;
-    int ID_4 = 4;
-
-
-    public static void readAR(KiboRpcApi api){
+    public static int[] readAR(KiboRpcApi api){
+        Mat matSrc = new Mat();
+        Mat IDs = new Mat();
+        List<Mat> Corners = new ArrayList<>();
+        Dictionary Dict = Aruco.getPredefinedDictionary(Aruco.DICT_5X5_250);
+        int[] AR_ID = new int[4];
         matSrc = api.getMatNavCam();
 
-        Aruco.detectMarkers(matSrc, Dict, Corners, IDs); 	// AR tag detector
-        AR_ID = new int[] 									// Put ID of AR Code to Array of int.
+        Aruco.detectMarkers(matSrc, Dict, Corners, IDs);
+        AR_ID = new int[]
                 {
                         (int) IDs.get(0, 0)[0],
                         (int) IDs.get(1, 0)[0],
                         (int) IDs.get(2, 0)[0],
                         (int) IDs.get(3, 0)[0]
                 };
-    }
 
+        return AR_ID;
+
+    }
 }
