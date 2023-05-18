37006e664f6438be45a142c5aa428b5e25dca089
BKOPYZ <breakoff1530@gmail.com>
2023-05-19 02:12:52 +0700

    feat: add calculateNavCamQuaternion and calculateDockCamQuaternion in QuaternionCalculator

--- app/src/main/java/jp/jaxa/iss/kibo/utils/QuaternionCalculator.java
+++ app/src/main/java/jp/jaxa/iss/kibo/utils/QuaternionCalculator.java
@@ -18,8 +18,8 @@ public class QuaternionCalculator {
      * {@code z = k * sin(Θ/2)}
      * </pre>
      *
-     * @param pos    position of the Astrobee (pivot at center point)
-     * @param target position of the target
+     * @param pos    coordinate(x,y,z) of Astrobee of the Astrobee (pivot at center point)
+     * @param target coordinate(x,y,z) of Astrobee of the target
      * @return quaternion of astrobee that laser point to target
      */
     static public Quaternion calculateQuaternion(Point pos, Point target) {
@@ -87,9 +87,131 @@ public class QuaternionCalculator {
      * @param cos       cos of theta between vector R1 and R2
      * @return          value of x type double
      */
-        static public double calculateDistanceBetweenLaserToPoint(double normR1, double normR3, double cos){
+    public static double calculateDistanceBetweenLaserToPoint(double normR1, double normR3, double cos){
         return (-2 * normR1 * cos + Math.sqrt(Math.pow(2 * normR1 * cos, 2) - 4 * (Math.pow(normR1, 2) - Math.pow(normR3, 2)))) / 2;
     }
+
+    /**
+     * Calculate quaternion from NavCam vector to the target point
+     *
+     * @param pos       coordinate(x,y,z) of Astrobee of the Astrobee (pivot at center point)
+     * @param target    coordinate(x,y,z) of Astrobee of the target
+     * @return          quaternion to where NavCam point to target
+     */
+    public static Quaternion calculateNavCamQuaternion(Point pos, Point target){
+        //vector between point pos to target
+        double x1 = target.getX() - pos.getX();
+        double y1 = target.getY() - pos.getY();
+        double z1 = target.getZ() - pos.getZ();
+
+        //norm of <x1,y1,z1>
+        double normVec1 = Math.sqrt(Math.pow(x1, 2) + Math.pow(y1, 2) + Math.pow(z1, 2));
+
+        //norm of vector from pivot of astrobee to laser pos
+        double normLaser = Math.sqrt(Math.pow(0.1302, 2) + Math.pow(0.0572, 2) + Math.pow(0.1111, 2));
+
+        // value of vector of laser shoot form <xi,0j,0k>
+        double _cos = Math.cos(Math.acos(0.1302d / normLaser));
+        double extraX = calculateDistanceBetweenLaserToPoint(normLaser,normVec1,_cos);
+
+        //vector between pivot of Astrobee to final laser pos
+        double x2 = 0.1177d + extraX;
+        double y2 = -0.0422d;
+        double z2 = -0.0826d;
+
+        //norm of <x2,y2,z2>
+        double normVec2 = Math.sqrt(Math.pow(x2, 2) + Math.pow(y2, 2) + Math.pow(z2, 2));
+
+        //make 2 vector a unit vector
+        x1 /= normVec1;
+        y1 /= normVec1;
+        z1 /= normVec1;
+        x2 /= normVec2;
+        y2 /= normVec2;
+        z2 /= normVec2;
+
+        //find Θ from dot product
+        double theta = Math.acos(x1 * x2 + y1 * y2 + z1 * z2);
+
+        //find axis of rotation from cross product
+        double i = y2 * z1 - y1 * z2;
+        double j = -(x2 * z1 - x1 * z2);
+        double k = x2 * y1 - x1 * y2;
+
+        //norm of Axis
+        double normAxis = Math.sqrt(Math.pow(i, 2) + Math.pow(j, 2) + Math.pow(k, 2));
+
+
+        //unit vector of Axis
+        i /= normAxis;
+        j /= normAxis;
+        k /= normAxis;
+
+        //return quaternion
+        return new Quaternion((float) (i * Math.sin(theta / 2)), (float) (j * Math.sin(theta / 2)), (float) (k * Math.sin(theta / 2)), (float) Math.cos(theta / 2));
+
+    }
+    /**
+     * Calculate quaternion from DockCam vector to the target point
+     *
+     * @param pos       coordinate(x,y,z) of Astrobee of the Astrobee (pivot at center point)
+     * @param target    coordinate(x,y,z) of Astrobee of the target
+     * @return          quaternion to where DockCam point to target
+     */
+    public static Quaternion calculateDockCamQuaternion(Point pos, Point target){
+        //vector between point pos to target
+        double x1 = target.getX() - pos.getX();
+        double y1 = target.getY() - pos.getY();
+        double z1 = target.getZ() - pos.getZ();
+
+        //norm of <x1,y1,z1>
+        double normVec1 = Math.sqrt(Math.pow(x1, 2) + Math.pow(y1, 2) + Math.pow(z1, 2));
+
+        //norm of vector from pivot of astrobee to laser pos
+        double normLaser = Math.sqrt(Math.pow(0.1302, 2) + Math.pow(0.0572, 2) + Math.pow(0.1111, 2));
+
+        // value of vector of laser shoot form <xi,0j,0k>
+        double _cos = Math.cos(Math.acos(0.1302d / normLaser));
+        double extraX = calculateDistanceBetweenLaserToPoint(normLaser,normVec1,_cos);
+
+        //vector between pivot of Astrobee to final laser pos
+        double x2 = -0.1061d - extraX;
+        double y2 = -0.054d;
+        double z2 = -0.054d;
+
+        //norm of <x2,y2,z2>
+        double normVec2 = Math.sqrt(Math.pow(x2, 2) + Math.pow(y2, 2) + Math.pow(z2, 2));
+
+        //make 2 vector a unit vector
+        x1 /= normVec1;
+        y1 /= normVec1;
+        z1 /= normVec1;
+        x2 /= normVec2;
+        y2 /= normVec2;
+        z2 /= normVec2;
+
+        //find Θ from dot product
+        double theta = Math.acos(x1 * x2 + y1 * y2 + z1 * z2);
+
+        //find axis of rotation from cross product
+        double i = y2 * z1 - y1 * z2;
+        double j = -(x2 * z1 - x1 * z2);
+        double k = x2 * y1 - x1 * y2;
+
+        //norm of Axis
+        double normAxis = Math.sqrt(Math.pow(i, 2) + Math.pow(j, 2) + Math.pow(k, 2));
+
+
+        //unit vector of Axis
+        i /= normAxis;
+        j /= normAxis;
+        k /= normAxis;
+
+        //return quaternion
+        return new Quaternion((float) (i * Math.sin(theta / 2)), (float) (j * Math.sin(theta / 2)), (float) (k * Math.sin(theta / 2)), (float) Math.cos(theta / 2));
+
+    }
+
 
     /**
      * Calculate between two Quaternion order not necessary
