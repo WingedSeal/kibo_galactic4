98c39dda2b32077e88afb51147d1b15ff3801a51
Kasidit Sarnrak <otto.ksarnrak@gmail.com>
2023-05-18 21:21:34 +0700

    fix(ARReader) : fix readAR

--- app/src/main/java/jp/jaxa/iss/kibo/utils/ARReader.java
+++ app/src/main/java/jp/jaxa/iss/kibo/utils/ARReader.java
@@ -15,31 +15,26 @@ public class ARReader {
      * Mark that Numbers of TargetsID start from the upper right
      * and increase in a counterclockwise direction.
      *
-     * read AR from the Navcam type Mat
-     * get ID from
+     * read AR from the NavCam type Mat
+     *
      *
      *
      * @param api KiboRpcApi
-     * @return int[]
+     * @return int[] as the idTarget according to the rulebook
      */
 
 
     public static int[] readAR(KiboRpcApi api){
-        Mat matSrc = new Mat();
         Mat IDs = new Mat();
         List<Mat> Corners = new ArrayList<>();
         Dictionary Dict = Aruco.getPredefinedDictionary(Aruco.DICT_5X5_250);
-        int[] AR_ID = new int[4];
-        matSrc = api.getMatNavCam();
+        Mat matSrc = api.getMatNavCam();
 
         Aruco.detectMarkers(matSrc, Dict, Corners, IDs);
-        AR_ID = new int[]
-                {
-                        (int) IDs.get(0, 0)[0],
-                        (int) IDs.get(1, 0)[0],
-                        (int) IDs.get(2, 0)[0],
-                        (int) IDs.get(3, 0)[0]
-                };
+        int[] AR_ID = new int[Corners.size()];
+        for(int i=0; i<Corners.size(); i++) {
+            AR_ID[i] = (int) IDs.get(i, 0)[0];
+        }
 
         return AR_ID;
 
