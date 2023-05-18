6b87f654de7d0d6bb1a83a7d170c227dc1d4bf0c
WingedSeal <firm09719@gmail.com>
2023-05-19 00:04:21 +0700

    docs(utils): ARReader.readAR

--- app/src/main/java/jp/jaxa/iss/kibo/utils/ARReader.java
+++ app/src/main/java/jp/jaxa/iss/kibo/utils/ARReader.java
@@ -1,28 +1,24 @@
 package jp.jaxa.iss.kibo.utils;
 
 
+import jp.jaxa.iss.kibo.rpc.api.KiboRpcApi;
 import org.opencv.aruco.Aruco;
 import org.opencv.aruco.Dictionary;
 import org.opencv.core.Mat;
 
 import java.util.ArrayList;
 import java.util.List;
-import jp.jaxa.iss.kibo.rpc.api.KiboRpcApi;
 
 public class ARReader {
 
     /**
-     * Mark that Numbers of TargetsID start from the upper right
+     * Read AR from the NavCam type Mat and mark that Numbers of TargetsID start from the upper right
      * and increase in a counterclockwise direction.
      *
-     * read AR from the NavCam type Mat
-     *
-     *
-     *
      * @param api KiboRpcApi
-     * @return int[] as the TargetsID according to the rulebook
+     * @return targets' ids according to the rulebook
      */
-    public static int[] readAR(KiboRpcApi api){
+    public static int[] readAR(KiboRpcApi api) {
         Mat IDs = new Mat();
         List<Mat> Corners = new ArrayList<>();
         Dictionary Dict = Aruco.getPredefinedDictionary(Aruco.DICT_5X5_250);
@@ -30,7 +26,7 @@ public class ARReader {
 
         Aruco.detectMarkers(matSrc, Dict, Corners, IDs);
         int[] AR_ID = new int[Corners.size()];
-        for(int i=0; i<Corners.size(); i++) {
+        for (int i = 0; i < Corners.size(); i++) {
             AR_ID[i] = (int) IDs.get(i, 0)[0];
         }
 
