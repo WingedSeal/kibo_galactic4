0044a96babc762db9e354128a34e2ed94478ab90
MystericalGD <nattawin.ya@gmail.com>
2023-05-19 17:53:49 +0700

    feat: add `equals()` method for Node class

--- app/src/main/java/jp/jaxa/iss/kibo/pathfind/Node.java
+++ app/src/main/java/jp/jaxa/iss/kibo/pathfind/Node.java
@@ -1,12 +1,28 @@
 package jp.jaxa.iss.kibo.pathfind;
 
 import gov.nasa.arc.astrobee.types.Point;
-
+import java.util.Objects;
 /**
  * A special Point, Astrobee can pass through while pathfinding
  */
 public class Node extends Point {
     public Node(double x, double y, double z) {
         super(x, y, z);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Node) {
+            Node node = (Node) obj;
+            return (this.getX() == node.getX() &&
+                    this.getY() == node.getY() &&
+                    this.getZ() == node.getZ());
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getX(), getY(), getZ());
     }
 }
