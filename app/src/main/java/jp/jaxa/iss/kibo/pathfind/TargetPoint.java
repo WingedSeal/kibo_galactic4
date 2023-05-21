515b42440370b0fe19d8be795d4204d8ffe96478
Kasidit Sarnrak <otto.ksarnrak@gmail.com>
2023-05-21 21:28:16 +0700

    fix: change Threshold and positions for TargetPoints

--- app/src/main/java/jp/jaxa/iss/kibo/pathfind/TargetPoint.java
+++ app/src/main/java/jp/jaxa/iss/kibo/pathfind/TargetPoint.java
@@ -17,7 +17,7 @@ public class TargetPoint extends PathFindNode {
     private static final TargetPoint[] TARGET_POINTS = {
             new TargetPoint(11.2625, -10.58+0.85d, 5.3625, POI_Id.POINT_1, 1, 30),
             new TargetPoint(10.513384, -9.085172, 3.76203+0.85d, POI_Id.POINT_2, 2, 20),
-            new TargetPoint(10.6031, -7.71007, 3.76093+0.85d, POI_Id.POINT_3, 3, 40),
+            new TargetPoint(10.6031, -7.71007, 3.76093+0.80d, POI_Id.POINT_3, 3, 40),
             new TargetPoint(9.866984+0.85d, -6.673972, 5.09531, POI_Id.POINT_4, 4, 20),
             new TargetPoint(11.102, -8.0304, 5.9076-0.85d, POI_Id.POINT_5, 5, 30),
             new TargetPoint(12.023-0.85d, -8.989, 4.8305, POI_Id.POINT_6, 6, 30),
