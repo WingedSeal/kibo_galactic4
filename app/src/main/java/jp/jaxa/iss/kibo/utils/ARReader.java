cd196c30c79c5d89761b8ddbef166acfa73861fd
WingedSeal <firm09719@gmail.com>
2023-05-22 21:35:08 +0700

    refactor(ARReader): improve the code

--- app/src/main/java/jp/jaxa/iss/kibo/utils/ARReader.java
+++ app/src/main/java/jp/jaxa/iss/kibo/utils/ARReader.java
@@ -3,104 +3,100 @@ package jp.jaxa.iss.kibo.utils;
 
 import gov.nasa.arc.astrobee.types.Point;
 import gov.nasa.arc.astrobee.types.Quaternion;
-import gov.nasa.arc.astrobee.types.Vec3d;
 import jp.jaxa.iss.kibo.rpc.api.KiboRpcApi;
 import org.opencv.aruco.Aruco;
-import org.opencv.aruco.DetectorParameters;
 import org.opencv.aruco.Dictionary;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
-import org.opencv.imgproc.Imgproc;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ARReader {
     private static int[] AR_ID = new int[0];
-    private static Mat rVec = new Mat();
-    private static Mat tVec = new Mat();
-    private static final Point TopLeftToCenter = new Point(0.1d,-0.0375d,0);
-    private static final Point TopRightToCenter = new Point(-0.1d,-0.0375d,0);
-    private static final Point BotLeftToCenter = new Point(0.1,0.0375,0);
-    private static final Point BotRightToCenter = new Point(-0.1,0.0375,0);
+    private static final Point TOP_LEFT_TO_CENTER = new Point(0.1d, -0.0375d, 0);
+    private static final Point TOP_RIGHT_TO_CENTER = new Point(-0.1d, -0.0375d, 0);
+    private static final Point BOT_LEFT_TO_CENTER = new Point(0.1, 0.0375, 0);
+    private static final Point BOT_RIGHT_TO_CENTER = new Point(-0.1, 0.0375, 0);
+    private static final Point LASER_POINT_COORDINATE = new Point(0.0994d, 0.0285d, QuaternionCalculator.extraX);
+
     /**
      * Read AR from the NavCam type Mat and mark that Numbers of TargetsID start from the upper right
      * and increase in a counterclockwise direction.
      *
-     * @param api   KiboRpcApi
-     * @return      Corners(List<Mat>) of each AR that can detect from NavCam image type Mat
+     * @param api KiboRpcApi
+     * @return Corners(List < Mat >) of each AR that can detect from NavCam image type Mat
      */
     public static List<Mat> readAR(KiboRpcApi api) {
-        Mat IDs = new Mat();
-        List<Mat> Corners = new ArrayList<>();
-        Dictionary Dict = Aruco.getPredefinedDictionary(Aruco.DICT_5X5_250);
+        Mat idMatrix = new Mat();
+        List<Mat> corners = new ArrayList<>();
+        Dictionary dict = Aruco.getPredefinedDictionary(Aruco.DICT_5X5_250);
         Mat matSrc = api.getMatNavCam();
 
-        Aruco.detectMarkers(matSrc, Dict, Corners, IDs);
-        AR_ID = new int[Corners.size()];
-        for (int i = 0; i < Corners.size(); i++) {
-            AR_ID[i] = (int) IDs.get(i, 0)[0];
+        Aruco.detectMarkers(matSrc, dict, corners, idMatrix);
+        AR_ID = new int[corners.size()];
+        for (int i = 0; i < corners.size(); i++) {
+            AR_ID[i] = (int) idMatrix.get(i, 0)[0];
         }
 
-        return Corners;
+        return corners;
 
     }
 
 
-    public static Point calculateErrorCoordinate(KiboRpcApi api, Quaternion quaternion){
-        Point laserCoordinate = laserPointCoordinate();
+    public static Point calculateErrorCoordinate(KiboRpcApi api, Quaternion quaternion) {
+        Mat rVec = new Mat();
+        Mat tVec = new Mat();
         double[][] navCamInstinct = api.getNavCamIntrinsics();
         Mat cameraMatrix = new Mat(3, 3, CvType.CV_32FC1);
         Mat dstMatrix = new Mat(1, 5, CvType.CV_32FC1);
-        Mat objPoints =new Mat();
+        Mat objPoints = new Mat();
         cameraMatrix.put(0, 0, navCamInstinct[0]);
         dstMatrix.put(0, 0, navCamInstinct[1]);
-        final float ARUCOSIZE = 5f; //cm
-        List<Mat> corner = readAR(api);
-        Aruco.estimatePoseSingleMarkers(corner,ARUCOSIZE,cameraMatrix,dstMatrix,rVec,tVec,objPoints);
-        if(AR_ID.length != 0){
-            Point PointToTarget = null;
-            switch(AR_ID[0]%4) {
-                case 1:
-                    PointToTarget = TopRightToCenter;
-                    break;
-                case 2:
-                    PointToTarget = TopLeftToCenter;
-                    break;
-                case 3:
-                    PointToTarget = BotLeftToCenter;
-                    break;
-                case 0:
-                    PointToTarget = BotRightToCenter;
-                    break;
-                default:
-                    break;
-            }
-            double[] rotationVector = new double[]{rVec.get(0,0)[0],rVec.get(0,0)[1],rVec.get(0,0)[2]};
-            double radianOfRotation = Math.sqrt(Math.pow(rotationVector[0],2)+Math.pow(rotationVector[1],2)+ Math.pow(rotationVector[2],2));
-            rotationVector[0]/= radianOfRotation;
-            rotationVector[1]/= radianOfRotation;
-            rotationVector[2]/= radianOfRotation;
+        final float ARUCO_SIZE_CM = 5f;
+        List<Mat> corners = readAR(api);
+        Aruco.estimatePoseSingleMarkers(corners, ARUCO_SIZE_CM, cameraMatrix, dstMatrix, rVec, tVec, objPoints);
+        if (AR_ID.length == 0) return null;
 
-            Point vectorToTarget = QuaternionCalculator.rotateVector(rotationVector,PointToTarget,radianOfRotation);
-
-            Point idealTargetPoint = new Point(PointToTarget.getX() + vectorToTarget.getX() , PointToTarget.getY() + vectorToTarget.getY() , PointToTarget.getZ() + vectorToTarget.getZ());
-            Point moveTo = new Point(idealTargetPoint.getX() - laserCoordinate.getX() ,idealTargetPoint.getY() - laserCoordinate.getY() ,idealTargetPoint.getZ() - laserCoordinate.getZ() );
-            double[] currentQuaternionInfo = QuaternionCalculator.getInfoQuaternion(quaternion);
-            Point unfixedMoveTo = QuaternionCalculator.rotateVector(currentQuaternionInfo,moveTo,currentQuaternionInfo[3]);
-            Point fixedMoveTo =  new Point(-unfixedMoveTo.getZ(),unfixedMoveTo.getY(),-unfixedMoveTo.getY());
-            return fixedMoveTo;
+        Point pointToTarget;
+        switch (AR_ID[0] % 4) {
+            case 1:
+                pointToTarget = TOP_RIGHT_TO_CENTER;
+                break;
+            case 2:
+                pointToTarget = TOP_LEFT_TO_CENTER;
+                break;
+            case 3:
+                pointToTarget = BOT_LEFT_TO_CENTER;
+                break;
+            case 0:
+                pointToTarget = BOT_RIGHT_TO_CENTER;
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + AR_ID[0] % 4);
         }
+        double[] rotationVector = new double[]{rVec.get(0, 0)[0], rVec.get(0, 0)[1], rVec.get(0, 0)[2]};
+        double radianOfRotation = Math.sqrt(Math.pow(rotationVector[0], 2) + Math.pow(rotationVector[1], 2) + Math.pow(rotationVector[2], 2));
+        rotationVector[0] /= radianOfRotation;
+        rotationVector[1] /= radianOfRotation;
+        rotationVector[2] /= radianOfRotation;
 
-//        Point[] coordinateAR = new Point[corner.size()];
-//        for(int i=0; i<corner.size();++i){
-//            coordinateAR[i] = new Point(tVec.get(i,0)[0],tVec.get(i,0)[1],tVec.get(i,0)[2]);// [x,y,z]
-//        }
-//        return coordinateAR;
-        return null;
+        Point vectorToTarget = QuaternionCalculator.rotateVector(rotationVector, pointToTarget, radianOfRotation);
+
+        Point idealTargetPoint = new Point(
+                pointToTarget.getX() + vectorToTarget.getX(),
+                pointToTarget.getY() + vectorToTarget.getY(),
+                pointToTarget.getZ() + vectorToTarget.getZ()
+        );
+        Point moveTo = new Point(
+                idealTargetPoint.getX() - LASER_POINT_COORDINATE.getX(),
+                idealTargetPoint.getY() - LASER_POINT_COORDINATE.getY(),
+                idealTargetPoint.getZ() - LASER_POINT_COORDINATE.getZ()
+        );
+        double[] currentQuaternionInfo = QuaternionCalculator.getInfoQuaternion(quaternion);
+        Point unfixedMoveTo = QuaternionCalculator.rotateVector(currentQuaternionInfo, moveTo, currentQuaternionInfo[3]);
+        return new Point(-unfixedMoveTo.getZ(), unfixedMoveTo.getY(), -unfixedMoveTo.getY());
     }
-    private static Point laserPointCoordinate(){
-        return new Point(0.0994d,0.0285d,QuaternionCalculator.extraX);
-    }
+
 
 }
