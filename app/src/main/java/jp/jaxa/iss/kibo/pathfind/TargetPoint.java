29a7b2be80d7c898956f24cfeddbf12a4aae0f7b
Kasidit Sarnrak <otto.ksarnrak@gmail.com>
2023-06-08 00:35:19 +0700

    fix : change value in TargetPoint

--- app/src/main/java/jp/jaxa/iss/kibo/pathfind/TargetPoint.java
+++ app/src/main/java/jp/jaxa/iss/kibo/pathfind/TargetPoint.java
@@ -15,13 +15,13 @@ public class TargetPoint extends PathFindNode {
 
 
     private static final TargetPoint[] TARGET_POINTS = {
-            new TargetPoint(11.2681, -9.73-0.05d, 5.3625-0.28d, POI_Id.POINT_1, 1, 30),
-            new TargetPoint(10.513384, -9.085172, 3.76203+0.90d, POI_Id.POINT_2, 2, 20),
-            new TargetPoint(10.6031, -7.71007, 3.76093+0.80d, POI_Id.POINT_3, 3, 40),
-            new TargetPoint(10.402, -6.8406, 5.0825, POI_Id.POINT_4, 4, 20),
-            new TargetPoint(11.102, -8.0304, 5.9076-0.828d, POI_Id.POINT_5, 5, 30),
-            new TargetPoint(12.023-0.85d, -8.989, 4.8305, POI_Id.POINT_6, 6, 30),
-            new TargetPoint(11.369, -8.5518, 4.48, POI_Id.POINT_7, 7 , 20),
+            new TargetPoint(11.2681d, -9.73d-0.05d, 5.3625d-0.28d, POI_Id.POINT_1, 1, 30),
+            new TargetPoint(10.513384d, -9.085172d, 3.76203d+0.90d, POI_Id.POINT_2, 2, 20),
+            new TargetPoint(10.6031d, -7.71007d, 3.76093d+0.80d, POI_Id.POINT_3, 3, 40),
+            new TargetPoint(10.402d, -6.8406d, 5.0825, POI_Id.POINT_4, 4, 20),
+            new TargetPoint(11.102d, -8.0304d, 5.9076d-0.82d, POI_Id.POINT_5, 5, 30),
+            new TargetPoint(12.023d-0.85d, -8.989d, 4.8305, POI_Id.POINT_6, 6, 30),
+            new TargetPoint(11.369d, -8.5518d, 4.48d, POI_Id.POINT_7, 7 , 20),
     };
 
     public static TargetPoint getTargetPoint(int pointNumber) {
